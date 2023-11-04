bd6af85609d3aeb128f25ba424c6ff9a026cee06
ayaelas <assalharras@gmail.com>
2023-11-04 07:12:21 -0700

    e

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+my_list = [1, 2, 3, 4, 5]
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{}".format(i)) 
+        i + 1
+
+print_list_integer(my_list)

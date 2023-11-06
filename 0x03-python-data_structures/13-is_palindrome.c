96a1e4b73fe859879996c0cf8dad5ab1721d7d5c
ayaelas <assalharras@gmail.com>
2023-11-06 08:48:03 -0800

    r

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,53 @@
-#include <string.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_palindrome - functin
- * @head: start of list
- * Return: 1 suc or 0
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head;
-	listint_t *prev = NULL, *tmp;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL || head2->next == NULL)
 		return (1);
-
-	while (fast != NULL && fast->next != NULL)
+	while (head2 != NULL)
 	{
-		fast = fast->next->next;
-
-		tmp = slow->next;
-		slow->next = prev;
-		prev = slow;
-		slow = tmp;
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
 	}
-
-	if (fast != NULL)
-		slow = slow->next;
-
-	if (slow != NULL)
+	aux2 = aux;
+	while (*head != NULL)
 	{
-		if (slow->n != prev->n)
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
 			return (0);
-
-		slow = slow->next;
-		prev = prev->next;
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
 	}
+	free_listint(aux);
 	return (1);
 }

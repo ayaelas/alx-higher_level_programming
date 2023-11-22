bdfa3c750dbd7b8b63fe042ece40459a1938f849
ayaelas <assalharras@gmail.com>
2023-11-22 04:54:37 -0800

    added advanced

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -27,7 +27,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def are(self):
+    def area(self):
         """Return the current area of square. """
         return (self.__size * self.__size)
 

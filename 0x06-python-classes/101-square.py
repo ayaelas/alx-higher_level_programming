b6230fd443b5d94f71227e4c0f4ba56e86870fdc
ayaelas <assalharras@gmail.com>
2023-11-22 04:12:38 -0800

    classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+
+"""Define a class Square"""
+
 class Square:
+    """Represent a square"""
+
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of thenew square.
+            position (int, int): the position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -18,6 +30,7 @@ class Square:
 
     @property
     def position(self):
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -28,8 +41,11 @@ class Square:
                 not all (num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
     def area(self):
+        """Get/set the current area of the square."""
         return (self.__size * self.__size)
+
     def my_print(self):
         if self.__size == 0:
             print("")
@@ -42,6 +58,7 @@ class Square:
             print("")
 
     def __str__(self):
+        """define he print() representation of a square."""
         if self.__size != 0:
             [print("") for i in range(0,self.__position[1])]
         for  i in range(0, self.__size):

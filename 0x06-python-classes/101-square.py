aefd7609ddcfb9aa4ce0b50ea32db6d3292e1b9b
ayaelas <assalharras@gmail.com>
2023-11-22 04:45:34 -0800

    added advanced

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -2,6 +2,7 @@
 
 """Define a class Square"""
 
+
 class Square:
     """Represent a square"""
 
@@ -38,7 +39,7 @@ class Square:
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
-                not all (num >= 0 for num in value)):
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -60,8 +61,8 @@ class Square:
     def __str__(self):
         """define he print() representation of a square."""
         if self.__size != 0:
-            [print("") for i in range(0,self.__position[1])]
-        for  i in range(0, self.__size):
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:

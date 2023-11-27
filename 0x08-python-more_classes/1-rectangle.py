b6736e25f7b7470843879aaa1a71c8d576f7f9fe
ayaelas <assalharras@gmail.com>
2023-11-27 07:23:45 -0800

    r

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
-"""Defines an Rectangle class"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """ Rectangle Class"""
-    def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Represent a rectangle."""
 
-    @property
-    def width(self):
-        """
-        Get: Rectangle Width
-        """
+            def __init__(self, width=0, height=0):
+                        """Initialize a new Rectangle.
 
-        return self.__width
-    
-    @width.setter
-    def width(self, value):
-        """
-        set: Rectangle Width
-        """
+                                Args:
+                                            width (int): The width of the new rectangle.
+                                                        height (int): The height of the new rectangle.
+                                                                """
+                                                                        self.width = width
+                                                                                self.height = height
 
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+                                                                                    @property
+                                                                                        def width(self):
+                                                                                                    """Get/set the width of the rectangle."""
+                                                                                                            return self.__width
 
-    @property
-    def height(self):
-        """
-        Get: Rectangle height
-        """
+                                                                                                            @width.setter
+                                                                                                                def width(self, value):
+                                                                                                                            if not isinstance(value, int):
+                                                                                                                                            raise TypeError("width must be an integer")
+                                                                                                                                                if value < 0:
+                                                                                                                                                                raise ValueError("width must be >= 0")
+                                                                                                                                                                    self.__width = value
 
-        return self.__height
+                                                                                                                                                                        @property
+                                                                                                                                                                            def height(self):
+                                                                                                                                                                                        """Get/set the height of the rectangle."""
+                                                                                                                                                                                                return self.__height
 
-    @width.setter
-    def height(self, value):
-        """
-        set : Rectangle height
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+                                                                                                                                                                                                @height.setter
+                                                                                                                                                                                                    def height(self, value):
+                                                                                                                                                                                                                if not isinstance(value, int):
+                                                                                                                                                                                                                                raise TypeError("height must be an integer")
+                                                                                                                                                                                                                                    if value < 0:
+                                                                                                                                                                                                                                                    raise ValueError("height must be >= 0")
+                                                                                                                                                                                                                                                        self.__height = value

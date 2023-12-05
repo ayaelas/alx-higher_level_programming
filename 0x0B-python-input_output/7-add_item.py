505e037ac1bbec22e79aefd47644b2514c64f613
ayaelas <assalharras@gmail.com>
2023-12-05 15:10:51 -0800

    freeze

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
-
 """
-Script that adds all arguments to a Python list.
+script that adds all arguments to a list
+and then save them to a file
 """
-
-import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-my_list = list(sys.argv[1:])
-
-try:
-    old_list = load_from_json_file('add_item.json')
-except Exception:
-    old_list = []
-
-old_list.extend(my_list)
-save_to_json_file(old_list, 'add_item.json')

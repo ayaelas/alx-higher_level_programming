0079cbdff24244763972479213eb1ffc448e19b6
ayaelas <assalharras@gmail.com>
2023-12-05 03:22:19 -0800

    freeze

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,7 +5,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Represent a square."""
-
     def __init__(self, size):
         """Initialize a new square.
 
@@ -15,3 +14,10 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+    
+    def area(self):
+        """Method for are of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[square] " + str(self.__size) + "/" + str(self.__size)

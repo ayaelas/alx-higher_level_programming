e492220593cf7557a00b2b42a7506b18d4c2dbf4
ayaelas <assalharras@gmail.com>
2023-12-05 14:28:06 -0800

    att

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """
 Module for Rectangle class.
 """
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 
 class Square(Rectangle):
@@ -18,6 +20,7 @@ class Square(Rectangle):
     def area(self):
         """Method for area of square."""
         return self.__size ** 2
+
     def __str__(self):
         """Returns string representation of this square."""
         return "[Square] " + str(self.__size) + "/" + str(self.__size)

0e3283cb8835a927ca58b348015db5542a32be33
ayaelas <assalharras@gmail.com>
2024-01-17 13:12:56 -0800

    advanced

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,5 @@
--- lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- lists all shows from hbtn_0d_tvshows_rate by their rating
+
 SELECT title, SUM(tv_show_rating.rate) AS rating
 FROM tv_shows
 LEFT JOIN tv_show_rating ON tv_show_rating.show_id = tv_shows.id

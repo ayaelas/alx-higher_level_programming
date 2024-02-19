2ea2fd228fdefab35a41b8d2a4adce5f9e595aef
ayaelas <assalharras@gmail.com>
2024-02-19 05:43:13 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+the class definition of a State
+and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="states")

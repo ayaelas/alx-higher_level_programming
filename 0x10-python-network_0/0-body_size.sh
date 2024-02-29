038731455675064d32055915379d4760facbdbf7
ayaelas <assalharras@gmail.com>
2024-02-29 12:11:55 -0800

    g

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL - Sends a request to that URL - Displa
+# Takes in a URL - Sends a request to that URL - Display
 curl -sI "$1" | grep 'Content-Length:' | cut -d ' ' -f2

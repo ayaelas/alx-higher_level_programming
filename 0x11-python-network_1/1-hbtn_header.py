4333730afe503deaf194e16b5dde7df2000f97d0
ayaelas <assalharras@gmail.com>
2024-03-01 04:33:33 -0800

    fr

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  sends a request to the URL
+"""
+
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        val = response.getheader('X-Request-Id')
+        print(val)
+

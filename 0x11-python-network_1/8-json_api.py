383bc7e6d0b0a75ba2d118393abf839ae78912e2
ayaelas <assalharras@gmail.com>
2024-03-01 04:53:37 -0800

    8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script takes a letter
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {"q": letter}
+
+    r = requests.post(url, data=data)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
+

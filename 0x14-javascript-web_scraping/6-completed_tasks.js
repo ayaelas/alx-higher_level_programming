6316f47dd6b81dba1452028ef58b010feb263283
ayaelas <assalharras@gmail.com>
2024-03-26 04:44:29 -0700

    fs

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-request.get(process.argv[2], (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
